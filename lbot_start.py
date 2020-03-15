00d1dd54ce9d8c7bef9cc4b38cb76fbf6c0e1776
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-03-15 10:40:25 +0100

    Add slack post function
    Add function to send the POST to slack and utilize it in on_ready event message.

--- lbot_start.py
+++ lbot_start.py
@@ -20,11 +20,37 @@ comm_prefix = '>'
 # sets the command prefix that the bot will respond to
 bot = commands.Bot(command_prefix=comm_prefix)
 
+# Sends POST request to Slack webhook to post the msg to the Slack channel.
+def slack_post(msg):
+    
+    webhook_url = 'https://hooks.slack.com/services/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'
+    slack_data = {
+                 'text': msg,
+                 'username': 'lukasbot-discord',
+                 'icon_url': 'https://github.com/lukasgabriel/lukasbot/blob/master/media/avatar.png',
+                 'channel': '#lukasbot-discord'
+                 }
+
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
 # creates event that prints console output as soon as bot connects to discord
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
-
+    
+    msg = 'I\'ve just connected to Discord.'
+    
+    slack_post(msg)
+    
 # function that does the actual 'starting'
 def lbot():
     # load the global variable for the discord token from the env variables
