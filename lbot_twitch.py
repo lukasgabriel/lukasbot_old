f16a44b7741c45f3c0d35eefcf65f5223e66bf6c
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-03-29 09:23:51 +0200

    Create lbot_twitch.py
    Create lbot_twitch.py which sends requests to the twitch API.

---
+++ lbot_twitch.py
@@ -0,0 +1,75 @@
+# lbot_twitch.py
+
+import os
+from dotenv import load_dotenv
+import requests
+from secrets import token_urlsafe
+
+load_dotenv()
+
+try:
+    CALLBACK_URL = os.getenv('CALLBACK_URL')
+except:
+    print('Error: No callback URL found in environment variables!')
+    raise EnvironmentError
+
+try:
+    TWITCH_CLIENT_ID = os.getenv('TWITCH_CLIENT_ID')
+except:
+    print('Error: No Twitch API Client ID found in environment variables!')
+    raise EnvironmentError
+
+try:
+    TWITCH_CLIENT_SECRET = os.getenv('TWITCH_CLIENT_SECRET')
+except:
+    print('Error: No Twitch API Client secret found in environment variables!')
+    raise EnvironmentError
+
+TWITCH_API = 'https://api.twitch.tv/helix'
+
+def get_twitch_token():
+    TWITCH_AUTH_URL = 'https://id.twitch.tv/oauth2/token'
+    params = {'client_id': TWITCH_CLIENT_ID,
+              'client_secret': TWITCH_CLIENT_SECRET, 'grant_type': 'client_credentials'}
+
+    request = requests.post(TWITCH_AUTH_URL, params=params)
+    response = request.json()
+
+    print('Twitch API response code: ' + request.status)
+    return response
+
+
+def revoke_twitch_token(token):
+    TWITCH_REVOKE_URL = 'https://id.twitch.tv/oauth2/revoke'
+    params = {'client_id': TWITCH_CLIENT_ID,
+              'token': token}
+
+    request = requests.post(TWITCH_REVOKE_URL, params=params)
+    response = request
+    
+    print(response)
+    return response
+
+
+def twitch_sub2webhook(mode, topic, lease):
+    callback_target = CALLBACK_URL + '/twitchapi/webhooks/callback/'
+    TWITCH_WEBHOOK_HUB = TWITCH_API + '/webhooks/hub/'
+
+    temp_secret = token_urlsafe()
+    os.environ['TEMP_SECRET'] = temp_secret
+
+    params = {'hub.mode': mode, 'hub.topic': TWITCH_API + topic,
+              'hub.callback': callback_target, 'hub.lease_seconds': lease, 'hub.secret': temp_secret}
+
+    header = {'Content-Type': 'application/json',
+              'Client-ID': TWITCH_CLIENT_ID}
+
+    request = requests.post(TWITCH_WEBHOOK_HUB, headers=header, params=params)
+    response = request
+
+    print(params, header)
+    print(response)
+    return response
+
+
+twitch_sub2webhook('subscribe', '/users/follows?first=1&from_id=24238947', '300')
