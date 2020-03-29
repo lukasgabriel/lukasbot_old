1c21a65c79a02580732ab99b05d8664d23335e2c
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-03-29 06:05:51 +0200

    Add route for twitch webhook callback

--- lbot_web.py
+++ lbot_web.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/twitchapi/webhooks/callback/', methods=['GET', 'POST'])
+def twitch_webhook_callback():
+    return 
