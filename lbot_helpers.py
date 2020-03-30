25bcdea1cbc4da901c0e142caa695ec1cc0508d8
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-03-30 22:53:49 +0200

    Add response text to APIError class

--- lbot_helpers.py
+++ lbot_helpers.py
@@ -7,11 +7,12 @@ class Error(Exception):
 
 class APIError(Error):
     # Exception raised if an API call returns an HTTP error.
-    def __init__(self, code, url, headers, msg):
+    def __init__(self, code, url, headers, msg, text):
         self.code = code
         self.url = url
         self.headers = headers
         self.msg = msg
+        self.text = text
 
 class InputError(Error):
     # Exception raised for errors in the input.
