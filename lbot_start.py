8f45293c04524065f47a2d7a4b4012bde6dec5f9
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-04-01 09:57:12 +0200

    Update lbot_start.py

--- lbot_start.py
+++ lbot_start.py
@@ -92,7 +92,8 @@ def slack_post(msg):
 def send2webhook(msg):
     print(msg) # DEBUGGING
     webhook_target = f'{WEBHOOK_ROOT}{WEBHOOK_ID}/{WEBHOOK_TOKEN}'
-    response = requests.post(webhook_target, params={'content': msg}, headers={'Content-Type': 'application/json'})
+    data = {'content': msg}
+    response = requests.post(webhook_target, data=json.dumps(data), headers={'Content-Type': 'application/json'})
     if response.status_code != 200:
         raise lh.APIError(response.status_code, webhook_target,
                           response.headers, response.reason, response.text)
