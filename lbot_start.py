e98c4e36684f458161fdd2b78487f766b805b5a6
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-04-01 09:43:34 +0200

    Update lbot_start.py

--- lbot_start.py
+++ lbot_start.py
@@ -90,6 +90,7 @@ def slack_post(msg):
 
 # sends a message to a discord webhook
 def send2webhook(msg):
+    print(msg) # DEBUGGING
     webhook_target = f'{WEBHOOK_ROOT}{WEBHOOK_ID}/{WEBHOOK_TOKEN}'
     response = requests.post(webhook_target, data={'content': msg}, headers={'Content-Type': 'application/json'})
     if response.status_code != 200:
