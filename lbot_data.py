464018cdcfedfddd5d3877092caac0362151e0fb
Lukas Gabriel <52338810+lukasgabriel@users.noreply.github.com>
2020-04-02 16:35:31 +0200

    Create lbot_data.py

---
+++ lbot_data.py
@@ -0,0 +1,155 @@
+# lbot_data.py
+
+goodbottie = [
+    'Thank you :)',
+    'Aww, thanks.',
+    'I\'m just doing my job.',
+    'Glad I could help.',
+    'Don\'t mention it.',
+    'I\'m here to help.',
+    ':)',
+    '(っ◕‿◕)っ',
+    '( ˘ ³˘)♥',
+    'Thank you so much.',
+    'Thanks, I appreciate it.',
+    'Thank you!'
+]
+
+greetings_earlymorning = [
+    'Good morning, {author}.',
+    'Up early, {author}?'
+]
+greetings_morning = [
+    'Good morning, {author}.',
+    'Hey. Can\'t wait for lunch. What are you having?'
+]
+greetings_noon = [
+    'Nice to see you, {author}.',
+    'Hello, {author}'
+]
+greetings_afternoon = [
+    'Good afternoon, {author}.',
+    'Hey, {author}.'
+]
+greetings_evening = [
+    'Good evening, {author}.',
+    'Have you had a successful day, {author}?'
+]
+greetings_night = [
+    'Still awake, {author}?',
+    'Can\'t sleep, {author}?'
+]
+
+goodbyes = [
+    'Good-bye, {author}.',
+    'Farewell, {author}.',
+    'See you soon, {author}.',
+    'Stay safe, {author}.',
+    'Stay safe, {author}.',
+    'See you later, alligator!',
+    'See you in a while, crocodile!',
+    'Take care, {author}.'
+]
+
+about = 'I was made by the glorious @floymotive#1337 to serve him and his friends. He told me he would beat up anyone who is mean to me, so watch out. \n View my source code at https://github.com/lukasgabriel/lukasbot'
+
+mp_games = [
+    'Gang Beasts',
+    'Age of Empires: Definitive Edition',
+    'RAFT',
+    'Monster Hunter: World',
+    'League of Legends',
+    'Rocket League',
+    'Gwent',
+    'Terraria...with Mods',
+    'Escape from Tarkov',
+    'Temtem',
+    'Wolcen: Lords of Mayhem',
+    'skribbl.io',
+    'UNO',
+    'Cards Against Humanity',
+    'nothing, and go outside instead',
+    'nothing, ands study instead',
+    'Halo: Master Chief Collection',
+    'Tower Unite',
+    'Tabletop Simulator',
+    'Garry\'s Mod',
+    'Deep Rock Galactic',
+    'Counter-Strike: Global Offensive',
+    'DotA 2',
+    'Starbound',
+    'MORDHAU',
+    'ASTRONEER',
+    'Destiny 2',
+    'Modded Minecraft',
+    'Warframe',
+    'Unturned',
+    'Left 4 Dead 2',
+    'Portal 2 Co-Op',
+    'Apex Legends',
+    'Fortnite',
+    'Spellsworn',
+    'SCUM',
+    'Read Dead Redemption 2',
+    'ShellShock Live',
+    'Grand Theft Auto V',
+    'Darwin Project',
+    'BattleBlock Theater',
+    'SpeedRunners',
+    'OverCooked 2',
+    'Plants vs Zombies: Battle for Neighborville',
+    'Borderlands 3',
+    'Sea of Thieves',
+    'Divinity: Original Sin',
+    'Overwatch',
+    'Keep Talking and Nobody Explodes',
+    'Payday',
+    'Payday 2',
+    'Rainbow Six: Siege',
+    'TowerFall Ascension',
+    'Jackbox Party Game Series',
+    'Nidhogg',
+    'Octodad',
+    'Ultimate Chicken Horse',
+    'Screencheat',
+    'Call of Duty: Modern Warfare',
+    'Trouble in Terrorist Town',
+    'Mario Kart 8',
+    'Mario Party',
+    'Garry\'s Mod: Murder',
+    'PUBG',
+    'Super Smash Bros. Ultimate',
+    'Team Fortress 2',
+    'Don\'t Starve Together',
+    'Monday Night Combat',
+    'Battlefield 2',
+    'Star Wars: Battlefront (Classic)',
+    'GTA: San Andreas Multiplayer',
+    'Old School RuneScape',
+    'A Way Out',
+    'Half Life: Deathmatch',
+    'TES V: Skyrim - Multiplayer Mod',
+    'The Forest',
+    'ARK: Survival Evolved',
+    'Rust',
+    'Life is Feudal',
+    'Roblox',
+    'Habbo Hotel',
+    'GeoGuessr',
+    'MS Paint Adventures',
+    'Town of Salem',
+    'Valorant',
+    'Mount & Blade II: Bannerlord'
+]
+
+address_book = {
+    'Alex': '+4917621214313',
+    'Tommi': '+4915733709805',
+    'Lukas': '+491623424473',
+    'Gabri': '+4915732612200',
+    'Sascha': '+4915733350547',
+    'Svenja': '+491725923971',
+    'Tolga': '+4915783461039',
+    'Gafar': '+4917668547754',
+    'Felix': '+4917656761234'
+}
